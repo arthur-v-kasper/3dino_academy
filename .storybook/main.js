c09fafd84f309a79f4450241d01fbe80dbf2a4ec
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2021-05-22 18:11:26 -0300

    Add global style

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,19 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: [
+  "../src/stories/*.stories.(js|mdx)", 
+  "../src/components/*.stories.(js|mdx)",
+  "../src/styles/*.stories.(js|mdx)"],
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+    "@storybook/addon-viewport",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true,
+      },
+    },
+    "@storybook/addon-knobs",
+  ],
+};
