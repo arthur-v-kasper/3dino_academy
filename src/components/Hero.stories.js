3007bda49a2f0058edc2f8e49402a0efb1beb298
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2021-05-22 18:13:27 -0300

    Add Hero component

---
+++ src/components/Hero.stories.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { text } from "@storybook/addon-knobs";
+
+import Hero from "../components/Hero";
+import Dino1 from "../stories/assets/dino-01.jpg"
+import Dino2 from "../stories/assets/dino-02.jpg"
+import Heading from "../components/Heading"
+
+export default {
+  title: "Components/Hero",
+  componet: Hero,
+};
+
+export const withList = () => (
+  <Hero image={Dino1}>
+    <Heading>
+      <h1>
+        Venha conhecer tudo sobre <strong>Dinossauros</strong>
+        <br />  
+        no nosso Dino-Curso
+      </h1>
+    </Heading>
+    <ul>
+      <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+      <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+      <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+    </ul>
+  </Hero>
+);
+
+export const usage = () => (
+  <Hero image={Dino2}>
+    <h1>{text("Title","Venha conhecer tudo sobre Dinossauros no nosso Dino-Curso")}</h1>
+    <p>{text("Text","Melhor dino-curso ever!")}</p>
+  </Hero>
+);
---
+++ src/components/Hero.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {  render } from '@testing-library/react'
+
+import Hero from "./Hero"
+
+test("renders with children", () =>{
+  const { getByText } = render(
+    <Hero>
+      <p>Arthur V Kasper</p>
+    </Hero>
+  )
+
+  expect(getByText("Arthur V Kasper")).toBeInTheDocument();
+})
+
+
+test("renders image backgrounds", () =>{
+  const image = "http://teste/image.jpg"
+  const {getByTestId} = render(<Hero image={image} />);
+  
+  expect(getByTestId("hero")).toHaveStyleRule({
+    backgroundImage: image
+  })
+})
