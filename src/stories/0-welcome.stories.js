0432265e6251d48f1e0b009482230ad9216f71fb
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2021-05-22 18:16:46 -0300

    Add welcome and jest style

---
+++ src/stories/0-welcome.stories.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default {
+  title: "Introduction/Welcome",
+}
+
+export const toDinoCursos = () =>(
+  <div>
+    <h1>Dino-Cursos</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit
+    </p>
+  </div>
+)
