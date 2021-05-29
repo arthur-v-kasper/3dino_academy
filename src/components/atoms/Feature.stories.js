689d0d233b6030f5b85d2f9d376beb6aba8fde9b
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2021-05-29 10:31:22 -0300

    Add Featured component

--- src/components/atoms/Feature.stories.js
+++ src/components/atoms/Feature.stories.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { FaOtter } from "react-icons/fa"
+import { FaOtter, FaCheck, FaStudiovinari, FaCashRegister } from "react-icons/fa";
 
 import Feature from "./Feature";
+import Grid from "components/atoms/Grid";
 
 export default {
   title: "Components/Atoms/Feature",
@@ -9,5 +10,24 @@ export default {
 }
 
 export const usage = () => (
-  <Feature icon={< FaOtter />} title="Meu título">Meu conteúdo</Feature>
+  <Feature icon={< FaOtter />} title="Meu título">
+    <p>Non duis officia enim nulla cillum nostrud exercitation mollit</p>
+  </Feature>
+);
+
+export const withGrid = () => (
+  <Grid sm={2} md={4}>
+    <Feature icon={< FaOtter />} title="Conheça todas as eras">
+      <p>Non duis officia enim nulla cillum nostrud exercitation mollit</p>
+    </Feature>
+    <Feature icon={< FaCheck />} title="Aprenda tudo sobre os tipos">
+      <p>Non duis officia enim nulla cillum nostrud exercitation mollit</p>
+    </Feature>
+    <Feature icon={< FaStudiovinari />} title="Se torne um paleontólogo">
+      <p>Non duis officia enim nulla cillum nostrud exercitation mollit</p>
+    </Feature>
+    <Feature icon={< FaCashRegister />} title="Acessível">
+      <p>Non duis officia enim nulla cillum nostrud exercitation mollit</p>
+    </Feature>
+  </Grid>
 );
