dbe613585592f52298229d7e5c58550cf99925d9
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2021-05-31 22:14:24 -0300

    Add Accordion component

---
+++ src/components/atoms/Accordion.js
@@ -0,0 +1,73 @@
+import React, {useState} from "react";
+import PropTypes from "prop-types";
+import styled, {css} from "styled-components";
+
+import {MdExpandMore, MdExpandLess} from "react-icons/md";
+
+const Root = styled.div`
+  border: 1px solid #e0e0e0;
+  border-left: 5px solid ${props => props.theme.colors.primary.main};
+  background-color: #fff;
+
+  ${props => props.open && css` // adiciona css se item do accordin está aberto.
+    margin: 16px 0;
+  `}
+
+  &:first-child {
+    margin-top: 0;
+  }
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+const Head = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 16px;
+  cursor: pointer;
+
+  & h6 {
+    margin: 0;
+    flex: 1 // dessa forma o elemento ocupa o espaço todos, empurrando o icone para o final, mas detalhes na aula 138
+  }
+
+  & svg {
+    font-size: 1.25rem;
+  }
+`;
+
+const Body = styled.div`
+  padding: 0 16px 16px 16px;
+`;
+
+const Accordion = ({ title, children }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <Root open={open}>
+      <Head role="button" onClick={handleClick}>
+        <h6>{title}</h6>
+        {open ? <MdExpandLess /> : <MdExpandMore />}
+      </Head>
+      {open && <Body>{children}</Body>}
+    </Root>
+  );
+};
+
+Accordion.defaultProps = {
+  title: undefined,
+  children: undefined,
+};
+
+Accordion.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node
+};
+
+export default Accordion;
