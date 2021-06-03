e1ebdca3e0132b2aa50dc2e4bb3655ac064971c7
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2021-06-03 09:20:15 -0300

    fix some texts

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -57,7 +57,7 @@ const Home = () => (
         <Feature icon={< FaCheck />} title="Do zero ao avançado">
           <p>Non duis officia enim nulla cillum nostrud exercitation mollit</p>
         </Feature>
-        <Feature icon={< FaStudiovinari />} title="Aprenda a melhores técnicas">
+        <Feature icon={< FaStudiovinari />} title="Aprenda as melhores técnicas">
           <p>Non duis officia enim nulla cillum nostrud exercitation mollit</p>
         </Feature>
         <Feature icon={< FaCashRegister />} title="Acessível">
@@ -101,7 +101,7 @@ const Home = () => (
         <Accordion title="O acesso é vitalício?">
           Laboris velit ullamco velit qui duis eiusmod occaecat cillum laborum magna. Exercitation ipsum id esse minim exercitation deserunt ipsum nulla qui est cillum non sunt ex.
         </Accordion>
-        <Accordion title="As aulas são on-line ou gravadas?">
+        <Accordion title="O curso possui suporte aos alunos?">
           Laboris velit ullamco velit qui duis eiusmod occaecat cillum laborum magna. Exercitation ipsum id esse minim exercitation deserunt ipsum nulla qui est cillum non sunt ex.
         </Accordion>
       </AccordionGroup>
